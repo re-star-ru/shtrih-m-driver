60610a0b5d6e26386e6961599f2ebb9f04ffe96d
fess932 <fess932@gmail.com>
2020-10-01 08:09:22 +0300

    start implement socket port

--- internal/fiscalprinter/port/socketPort.go
+++ internal/fiscalprinter/port/socketPort.go
@@ -1,3 +1,11 @@
 package port
 
 //package com.shtrih.fiscalprinter.port; todo
+
+//_, err := net.Dial("tcp", "10.51.0.71:7778")
+//if err != nil {
+//	log.Println(err.Error())
+//}
+//type SocketPort struct {
+//	PortName
+//}
