2f8a8319c124d2dabffc101d7f3d90b59b3b601d
fess932 <fess932@gmail.com>
2020-10-06 12:23:40 +0300

    refactoring

--- test.go
+++ test.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/hex"
+	"errors"
+	"fmt"
 	"log"
 	"net"
 )
@@ -37,11 +39,15 @@ import (
 // 8 байт - контрольная сумма либо 9 байт для фискального принтера
 
 //02 начало команды 1 байт - начало команды
-//06 FF 01  - признак команда Фискального Накопителя (ФН) или 05 - признак команда Принтера (П)
+//FF 01  - признак команда Фискального Накопителя (ФН) или 05 - признак команда Принтера (П)
 //(00 - FF) команда
 //(00 - FF) пароль
 func main() {
-	status()
+	//println(hex.Dump(createCommand(30, ReadShortStatus)))
+	//println(hex.Dump(createCommand(30, FnReadStatus)))
+	//println(hex.Dump(createCommand(30, FnReadSerial)))
+
+	readSerial()
 }
 
 func createFrame(data []byte) []byte {
@@ -62,9 +68,23 @@ func createFrame(data []byte) []byte {
 	return frameBuf.Bytes()
 }
 
-func readShortStatusCommand(password uint32) []byte {
+const (
+	// команды принтера
+	ReadShortStatus = 16
+
+	// команды фискального накопителя
+	FnReadStatus = 65281
+	FnReadSerial = 65282
+)
+
+func createCommand(password uint32, command uint16) []byte {
 	dataBuffer := bytes.NewBuffer([]byte{})
-	dataBuffer.Write([]byte{0x10}) // write command
+
+	cb := make([]byte, 2)
+	binary.BigEndian.PutUint16(cb, command)
+	cb = bytes.TrimPrefix(cb, []byte{0})
+
+	dataBuffer.Write(cb)
 
 	passwordBinary := make([]byte, 4)
 	binary.LittleEndian.PutUint32(passwordBinary, password)
@@ -86,66 +106,209 @@ func ping() {
 	b, _ := rw.ReadByte()
 	switch b {
 	case 0x6:
-		log.Println("OK, need recive")
+		log.Println("OK, need recive, recive now")
+		rw.WriteByte(0x06)
+		rw.Flush()
 	case 0x15:
 		log.Println("OK, nothing to recive")
 	default:
+		rw.WriteByte(0x06)
+		rw.Flush()
 		log.Fatal("ERR, ping byte:", b)
 	}
-
-	rw.WriteByte(0x06)
-	rw.Flush()
 }
 
-func status() {
-	rss := readShortStatusCommand(30)
-	frame := createFrame(rss)
-	println(hex.Dump(frame))
+func readSerial() {
 	ping()
 
-	//sendFrame(frame)
-}
-
-func sendFrame(frame []byte) {
-	//t := time.Now()
-	con, err := net.Dial("tcp", "10.51.0.71:7778")
-	defer con.Close()
-
+	if err := sendCommand(30, FnReadStatus); err != nil {
+		log.Fatal(err)
+	}
+	data, err := reciveDataFromFrame()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	r := bufio.NewReader(bytes.NewReader(data))
+	cmdBin, err := r.ReadBytes(0)
+	println("cmdbin: ")
+	println(hex.Dump(cmdBin))
+
+	////
+	fsStatusByte, _ := r.ReadByte()
+	println("fs status byte:", fsStatusByte)
+
+	println("isConfigured:", fsStatusByte&1)
+	println("isFiscalModeOpened:", fsStatusByte&2)
+	println("isFiscalModeClosed:", fsStatusByte&4)
+	println("isTransferCompleted:", fsStatusByte&8)
+	println()
+
+	//isConfigured = BitUtils.testBit((long)this.code, 0)
+	//isFiscalModeOpened = BitUtils.testBit((long)this.code, 1)
+	//isFiscalModeClosed = BitUtils.testBit((long)this.code, 2)
+	//isTransferCompleted = BitUtils.testBit((long)this.code, 3)
+	/////
+
+	fsDocTypeByte, _ := r.ReadByte()
+	println("fs doc type:", fsDocTypeByte)
+
+	fsIsDocRecivedByte, _ := r.ReadByte()
+	println("fs is doc received:", fsIsDocRecivedByte)
+
+	fsIsDayOpenedByte, _ := r.ReadByte()
+	println("fs is day opened:", fsIsDayOpenedByte)
+
+	fsFlagsByte, _ := r.ReadByte()
+	println("fs flags:", fsFlagsByte)
+
+	fsDateYearByte, _ := r.ReadByte()
+	fsDateMounthByte, _ := r.ReadByte()
+	fsDateDayByte, _ := r.ReadByte()
+	println("year, mounth, day:", fsDateYearByte, fsDateMounthByte, fsDateDayByte)
+
+	fsTimeHourByte, _ := r.ReadByte()
+	fsTimeMinByte, _ := r.ReadByte()
+	println("hour, minute:", fsTimeHourByte, fsTimeMinByte)
+
+	fsSerialBytes := make([]byte, 16)
+	r.Read(fsSerialBytes)
+	println("fs serial: ")
+	print(hex.Dump(fsSerialBytes))
+
+	fsLastDocNumber := make([]byte, 4)
+	r.Read(fsLastDocNumber)
+
+	println("fs last doc number: ", binary.LittleEndian.Uint32(fsLastDocNumber))
+	print(hex.Dump(fsLastDocNumber))
+}
+
+func sendCommand(password uint32, command uint16) error {
+	cmdBinary := createCommand(password, command)
+	frame := createFrame(cmdBinary)
+	println(hex.Dump(frame))
+
+	if err := sendFrame(frame); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sendFrame(frame []byte) error {
+	con, err := net.Dial("tcp", "10.51.0.71:7778")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer con.Close()
+
 	rw := bufio.NewReadWriter(bufio.NewReader(con), bufio.NewWriter(con))
 	rw.Write(frame)
 	rw.Flush()
 
-	_, err = rw.ReadByte()
+	b, err := rw.ReadByte()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//log.Println("Control byte:", b)
-	//log.Println("hex:")
-	//println(hex.Dump([]byte{b}))
-	//
-	//res2 := make([]byte, 32)
-	//n2, err := rw.Read(res2)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//log.Println("num:", n2)
-	//log.Println("buf:", res2)
-	//log.Println("hex:")
-	//println(hex.Dump(res2))
-	//
-	//rw.WriteByte(byte(0x06))
-	//rw.Flush()
-	//log.Println(time.Since(t))
+	switch b {
+	case 0x06:
+		return nil
+	case 0x15:
+		return errors.New("21, nothig to recive")
+	default:
+		return errors.New(fmt.Sprint("control byte is:", b, ": wft?"))
+	}
 }
+
+func checkCRC(data []byte, rcrc byte) error {
+	dlen := len(data)
+
+	crc := byte(dlen)
+	for i := 0; i < dlen; i++ {
+		crc ^= data[i]
+	}
+
+	if rcrc != crc {
+		return errors.New("crc does not match")
+	}
+
+	return nil
+}
+
+func reciveDataFromFrame() ([]byte, error) {
+	con, err := net.Dial("tcp", "10.51.0.71:7778")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rw := bufio.NewReadWriter(bufio.NewReader(con), bufio.NewWriter(con))
+	defer func() {
+		rw.WriteByte(byte(0x06))
+		rw.Flush()
+
+		con.Close()
+	}()
+
+	var frame bytes.Buffer
+
+	stx, _ := rw.ReadByte() // read byte STX (0x02) err need
+	println("stx byte:", stx)
+
+	dlen, _ := rw.ReadByte() // read byte dataLen (0x02)
+	println("data len:", dlen)
+
+	data := make([]byte, dlen)
+	n, _ := rw.Read(data)
+	println("read data bytes len:", n)
+
+	crc, _ := rw.ReadByte() // read crc byte
+	println("crc:", crc)
+
+	frame.WriteByte(stx)
+	frame.WriteByte(dlen)
+	frame.Write(data)
+	frame.WriteByte(crc)
+	log.Println("<- recive frame")
+	print(hex.Dump(frame.Bytes()))
+
+	if err := checkCRC(data, crc); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// fn read status
+// 02 06 FF 01 1E 00 00 00 E6
+// 02 | 21 | FF | 01 | 00 | 03 00 00 01 00 14 0A 06 0B 1A 39 32 38 31 30 30 30 31 30 30 30 30 37 34 34 32 3C 2E 00 00 | C0
+
+// fn read status go
+// 02 06 ff 01 1e 00 00 00 e6
+// stx| len|   cmd   | nil|   data 																					  |	crc
+// 02 | 21 | ff | 01 | 00 | 03 00 00 01 00 14 0a 06 0b 1a 39 32 38 31 30 30 30 31 30 30 30 30 37 34 34 32 3c 2e 00 00 | c0
+
+//----------------------------------------
+//Состояние фазы жизни    : 03h, 3
+//----------------------------------------
+//Проведена настройка ФН: [да]
+//Открыт фискальный режим: [да]
+//Закрыт фискальный режим: [нет]
+//Закончена передача фискальных данных в ОФД: [нет]
+//----------------------------------------
+//Текущий документ        : 00h, Нет открытого документа
+//Данные документа        : 00h, Нет данных документа
+//Состояние смены         : 01h, Смена открыта
+//Флаги предупреждения    : 00h, 0
+//Дата и время ФН         : 06.10.2020 11:26:00
+//Номер ФН                : 9281000100007442
+//Номер последнего ФД     : 11836
 
 //02 | 10 | 10 | 00 | 1E 92 02 02 00 00 9F E5 18 01 00 89 08 00 | 6E
 //02 | 10 | 10 | 00 | 1e 92 02 02 00 00 9f e5 18 01 00 89 08 00 | 6e
 //02 | 10 | 10 | 00 | 1E 92 02 02 00 00 9F E5 18 01 00 89 08 00 | 6E
-
+//					   (delim)
+//stx  19-(1 comnd 2) 3 - null   4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 | crc
+//02 | 13 | FF 02    |   00   | 39 32 38 31 30 30 30 31 30 30 30 30 37 34 34 32 | E8
+//02 | 13 | ff 02    |   00   | 39 32 38 31 30 30 30 31 30 30 30 30 37 34 34 32 | e8
 //----------------------------------------
 //Краткий запрос состояния:
 //----------------------------------------
