011d2e69cb850ca14e56e15f30ce65a7052077e2
fess932 <fess932@gmail.com>
2020-11-18 16:52:39 +0300

    refactoring, todo: Close, Open Shift

--- pkg/driver/printer/usecase.go
+++ pkg/driver/printer/usecase.go
@@ -4,13 +4,14 @@ import (
 	"github.com/fess932/shtrih-m-driver/pkg/driver/models"
 )
 
-const PrinterTimeout = 9900 // Дефолтный таймаут ожидания ККТ
+// const PrinterTimeout = 9900 // Дефолтный таймаут ожидания ККТ
 
 type Usecase interface {
 
 	// TODO: OpenShift, CloseShift
-	OpenShift()            // Открыть смену
-	CloseShift()           // Закрыть смену
+	OpenShift()  // Открыть смену
+	CloseShift() // Закрыть смену
+
 	ReadShortStatus() byte // Прочитать короткий статус, получить статус
 
 	AddOperationToCheck(op models.Operation) // Добавть операцию в чек
