d11d2af40e90c7553d6965ad57544bafa0078d9a
fess932 <fess932@gmail.com>
2021-09-07 15:43:31 +0300

    fix double hex cmd log

--- app/kkt/cmd.go
+++ app/kkt/cmd.go
@@ -10,14 +10,15 @@ import (
 )
 
 func (kkt *KKT) parseCmd(cmd []byte) error {
+	if cmd[0] == 0xFF {
+		return parseFNcmd(cmd[1:], kkt)
+	}
+
 	if cmd[0] != 0x10 {
 		log.Print("parce cmd:")
 		log.Print(hex.Dump(cmd))
 	}
 
-	if cmd[0] == 0xFF {
-		return parseFNcmd(cmd[1:], kkt)
-	}
 	if cmd[1] != 0x00 {
 		return errCheck(cmd[1])
 	}
